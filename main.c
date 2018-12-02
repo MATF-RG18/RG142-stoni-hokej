a70a574880a8ecbe99feb6a538a57f09c2c3de6a
AndjelaKarakas <andjela.karakas@gmail.com>
2018-12-02 20:38:30 +0100

    Dodato je osvetljenje

--- main.c
+++ main.c
@@ -35,6 +35,9 @@ int main(int argc, char **argv)
     glEnable(GL_DEPTH_TEST);
     glLineWidth(2);
 
+    /* Ukljucujemo normalizaciju vektora normala */
+    glEnable(GL_NORMALIZE);
+
     /* Program ulazi u glavnu petlju. */
     glutMainLoop();
 
@@ -107,6 +110,32 @@ void draw_table(float height, float radius_top, float radius_bottom)
 
 static void on_display(void)
 {
+
+    /* Pozicija svetla (u pitanju je direkcionalno svetlo). */
+    GLfloat light_position[] = { 0, 2, 1, 0 };
+
+    /* Ambijentalna boja svetla. */
+    GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1 };
+
+    /* Difuzna boja svetla. */
+    GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
+
+    /* Spekularna boja svetla. */
+    GLfloat light_specular[] = { 0.9, 0.9, 0.9, 1 };
+
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.7, 1 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 1, 1, 0, 1 };
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 1, 1, 1, 1 };
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 30;
+
+
     /* Brise se prethodni sadrzaj prozora. */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -119,6 +148,24 @@ static void on_display(void)
             0, 0, 0,
             0, 1, 0
         );
+
+
+    /* Ukljucuje se osvjetljenje i podesavaju parametri svetla. */
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+
 
     /* Crtaju se noge stola */
     glPushMatrix();
